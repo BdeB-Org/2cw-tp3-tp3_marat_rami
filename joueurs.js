ae4479b0fe9c07500f20fd36447e58ac88b8e747
Marat Sargsyan <marat5059@gmail.com>
2024-05-29 23:58:52 -0400

    Method pour formater la facon daffichage des dates

--- joueurs.js
+++ joueurs.js
@@ -1,7 +1,14 @@
+function formatDate(dateString) {
+    const date = new Date(dateString);
+    const options = { year: 'numeric', month: 'short', day: '2-digit' };
+    return date.toLocaleDateString('en-US', options);
+}
+
 fetch('http://localhost:8080/ords/hr2/joueurs')
 .then(response => response.json())
 .then(data => {
     const joueursTableBody = document.getElementById('joueursTableBody');
+
     data.items.forEach(joueurs => {
         const row = document.createElement('tr');
         row.innerHTML = `
@@ -9,7 +16,7 @@ fetch('http://localhost:8080/ords/hr2/joueurs')
                      <td>${joueurs.nom_joueur}</td>
                      <td>${joueurs.position}</td>
                      <td>${joueurs.numero_maillot}</td>
-                     <td>${joueurs.date_naissance}</td>
+                     <td>${formatDate(joueurs.date_naissance)}</td>
                      <td>${joueurs.contact}</td>
                  `;
                  joueursTableBody.appendChild(row);
